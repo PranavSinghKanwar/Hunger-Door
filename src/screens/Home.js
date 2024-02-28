bffa1300774dfd1f4a6d85b55848d4ac85cceb61
Pranav <pranavkanwar4@gmail.com>
2024-02-28 14:46:07 +0530

    added some componenets

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 function Home() {
   return (
     <div>
       <Navbar/>
+      <Carousel />
+      <div>
+        <Card />
+      </div>
       <Footer />
     </div>
   )
